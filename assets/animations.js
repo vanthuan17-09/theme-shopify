9168e0ffbbf4b7b6feaae5edbafd8a0929a4c028
vanthuan17-09 <vanthuan170902@gmail.com>
2024-10-31 16:49:30 +0700

    password-123

--- assets/animations.js
+++ assets/animations.js
@@ -2,7 +2,6 @@ const SCROLL_ANIMATION_TRIGGER_CLASSNAME = 'scroll-trigger';
 const SCROLL_ANIMATION_OFFSCREEN_CLASSNAME = 'scroll-trigger--offscreen';
 const SCROLL_ZOOM_IN_TRIGGER_CLASSNAME = 'animate--zoom-in';
 const SCROLL_ANIMATION_CANCEL_CLASSNAME = 'scroll-trigger--cancel';
-
 // Scroll in animation logic
 function onIntersection(elements, observer) {
   elements.forEach((element, index) => {
